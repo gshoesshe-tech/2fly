1ce6b64fbabc4f810833679bf5f7bdef52f5a58d
gshoesshe-tech <gshoesshe@gmail.com>
2026-01-05 15:40:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // IMPORTANT: fill these in before deploying.
 // Get them from Supabase Project Settings -> API
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://avghchbpqnuyvtqijhss.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2Z2hjaGJwcW51eXZ0cWlqaHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0NjcyMjQsImV4cCI6MjA4MzA0MzIyNH0.JxzbXI05PRa328NnGVA5C2o1iqsQ9Ey1Qh1yZ8GKjbk"
+  SUPABASE_URL: "https://tvzhfmusecrmgexcepmw.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2emhmbXVzZWNybWdleGNlcG13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1OTg2NjQsImV4cCI6MjA4MzE3NDY2NH0.84XuhnTXKCTZ2AH_c-f1tBcv2LaqLjLU1hnlI_9Smp4"
 };
